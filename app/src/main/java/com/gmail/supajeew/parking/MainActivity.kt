03ec6707f67d7f455471d33cec584838944a623d
lookpearsw <supajeew@gmail.com>
2019-08-21 19:34:54 +0700

    Add hint text

---
+++ app/src/main/java/com/gmail/supajeew/parking/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.gmail.supajeew.parking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
